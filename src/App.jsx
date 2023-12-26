c70a95644e8ea342634e01c9cdec0c8e7ec10080
RizukiBoy <teknologinformasi23@gmail.com>
2023-12-26 15:47:07 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,155 @@
+
+function App() {
+
+  return (
+    <>
+    <section className="bg-[#0D2A3F] text-white font-monserrat">
+    <nav className="container mx-auto border-b-2">
+      <div className="flex justify-between p-4 items-center">
+
+      <h1 className="font-semibold text-2xl font-mono">Rizuki Web</h1>
+
+      <div className="hidden md:flex items-center gap-4 font-semibold">
+        <a href="#" className="hover:text-teal-500">About</a>
+        <a href="#" className="hover:text-teal-500">Blog</a>
+        <a href="#" className="hover:text-teal-500">Contact</a>
+        <a href="#" className="hover:text-teal-500">Others</a>
+
+      <input type="text" className="bg-transparent p-2 outline-none border gray-500 rounded-md font-medium" placeholder='Search'/>
+      </div>
+
+      </div>
+    </nav>
+
+    <div className="w-full md:w-[40%] mt-20 mx-auto flex justify-evenly text-xs md:text-sm items-center">
+    
+      <span className="flex gap-2 items-center">
+        <ion-icon name="calendar"></ion-icon>
+        <p>December 27, 2023</p>
+      </span>
+
+      <span className="flex gap-2 items-center">
+        <ion-icon name="chatboxes"></ion-icon>
+        <p>33 Comments</p>
+      </span>
+
+
+      <span className="flex gap-2 items-center">
+        <ion-icon name="heart"></ion-icon>
+        <p>10 Likes</p>
+      </span>
+
+
+
+    </div>
+
+    <div className="">
+      <p className="my-10 font-semibold text-[40px] text-center">
+        Teknologi Untuk Semua Generasi
+      </p>
+
+      <div className="w-[90%] md:w-[70%] lg:w-[40%] mx-auto text-justify text-lg">
+        <p>Kemajuan teknologi telah menjadi pendorong utama perubahan masyarakat dalam beberapa dedade terakhir. Melalui tulisan ini, Saya akan membahas bagaimana teknologi bagi masa depan kita. Apa saja penerapannya dalam kehidupan setiap generasi, dan tantangan yang juga harus dihadapi terkait perkembangan teknologi yang begitu pesat.</p>
+
+        <h1 className="font-bold text-2xl my-6">Peran</h1>
+
+        <p>
+        <h1 className="font-semibold my-2">
+          -Membuka Akses Pendidikan untuk Semua Generasi
+        </h1>
+
+        Salah satu keberhasilan terbesar teknologi adalah membuka akses pendidikan untuk semua generasi. Melalui platform daring, siapa pun dari berbagai lapisan usia dapat mengakses sumber daya pendidikan tanpa batasan geografis. Baik anak-anak yang masih duduk di bangku sekolah maupun generasi yang lebih tua yang ingin terus belajar, teknologi telah menghapuskan hambatan dan menciptakan peluang baru bagi semua.
+
+        <h1 className="font-semibold my-2">
+        - Menjembatani Kesenjangan Generasi Melalui Media Sosial
+        </h1>
+
+        Media sosial menjadi jembatan yang kuat dalam menghubungkan berbagai generasi. Kini, nenek dan kakek dapat terhubung dengan cucu mereka melalui video call, berbagi momen, dan mengikuti perkembangan kehidupan sehari-hari satu sama lain. Dengan demikian, teknologi menciptakan ruang bagi pembelajaran saling memahami antar-generasi dan mengurangi kesenjangan yang mungkin muncul karena perbedaan pengalaman.
+
+        <h1 className="font-semibold my-2">
+        - Menyediakan Solusi Inovatif untuk Kesehatan dan Kesejahteraan
+        </h1>
+
+        Perkembangan teknologi di bidang kesehatan telah mengubah cara kita menjaga kesehatan. Dari aplikasi pelacak kesehatan hingga telemedicine, teknologi memungkinkan semua generasi untuk mengakses layanan kesehatan secara lebih efisien. Ini bukan hanya memudahkan akses bagi generasi muda yang sibuk, tetapi juga memberikan solusi bagi generasi tua yang mungkin sulit untuk mengakses fasilitas kesehatan secara langsung.
+
+        <h1 className="font-semibold my-2">
+        - Pemeliharaan Budaya dan Warisan Melalui Digitalisasi
+        </h1>
+
+        Teknologi juga memiliki peran dalam memelihara dan menyebarkan warisan budaya dari satu generasi ke generasi berikutnya. Melalui proyek digitalisasi, dokumentasi, dan pengarsipan daring, kita dapat memastikan bahwa pengetahuan dan tradisi dari generasi sebelumnya tetap hidup dan dapat diakses oleh generasi mendatang.
+
+        <h1 className="font-semibold my-2">
+        - Menciptakan Peluang Ekonomi untuk Semua Usia
+        </h1>
+          
+
+        Peran teknologi dalam menciptakan peluang ekonomi juga sangat penting. Generasi tua dapat memanfaatkan teknologi untuk berkontribusi dalam berbagai bidang, termasuk freelancing online, mentoring melalui platform daring, dan berpartisipasi dalam ekosistem digital yang terus berkembang. Ini memberikan bukti bahwa teknologi bukan hanya milik generasi muda, tetapi dapat memberdayakan semua lapisan masyarakat.
+
+        <p className="mt-6">
+        Dengan demikian, teknologi bukan hanya sebuah tren atau alat modern, tetapi sebuah kekuatan penyatuan yang dapat mempersatukan semua generasi. Melalui pemanfaatan teknologi dengan bijak, kita dapat menciptakan masa depan yang lebih inklusif, di mana setiap generasi dapat merasakan manfaat dan kontribusi yang mereka bawa ke meja pembangunan masyarakat global. Mari kita bersama-sama merangkul masa depan, membuka peluang baru, dan menjadikan teknologi sebagai alat untuk mempersatukan, bukan memisahkan.
+        </p>
+
+        </p>
+
+        <h1 className="font-bold text-2xl my-6">Tantangan</h1>
+
+        <p>
+        Perkembangan teknologi yang begitu pesat membawa dampak positif yang tak terbantahkan. Namun,
+        Dalam setiap tahap kehidupan, setiap generasi dihadapkan pada tantangan yang muncul seiring dengan pesatnya perkembangan teknologi. 
+
+        <h1 className="font-semibold my-2"> 
+        - Keselamatan dan Paparan Berlebihan pada Teknologi
+        </h1>
+
+        Tantangan pertama terletak pada keselamatan dan paparan berlebihan pada teknologi. Kesenjangan digital juga dapat terjadi kepada anak-anak yang memiliki akses dan yang tidak. Sementara paparan pada konten yang tidak sesuai dapat berdampak pada perkembangan mental dan emosional mereka. Pengasuhan digital dan pemantauan yang bijak menjadi kunci untuk melindungi anak-anak dari potensi risiko ini.
+
+        <h1 className="font-semibold my-2">
+        - Tekanan dan Kesenjangan Sosial Digital
+        </h1>
+  
+
+        Tekanan sosial digital menjadi salah satu tantangan utama. Dalam upaya untuk mencocokkan standar kecantikan atau kesuksesan yang ditampilkan secara online, remaja mungkin mengalami tekanan mental dan rendahnya harga diri. Selain itu, ancaman siber seperti perundungan online dan penipuan dapat mempengaruhi kesejahteraan mereka. Pendidikan tentang etika digital dan dukungan sosial sangat penting dalam membantu remaja menghadapi tantangan ini.
+
+        <h1 className="font-semibold my-2">
+        - Pengangguran dan Tuntutan Produktivitas yang Tinggi
+        </h1>
+
+        Teknologi dapat membawa tantangan terkait pengangguran akibat otomatisasi dan tuntutan produktivitas yang tinggi. Beberapa pekerja mungkin merasa tertinggal oleh perubahan teknologi dan memerlukan pelatihan keterampilan baru untuk tetap relevan di dunia kerja yang terus berkembang. Keseimbangan antara kehidupan kerja dan kehidupan pribadi juga menjadi perhatian, di mana teknologi dapat menciptakan batasan yang kabur antara keduanya.
+
+        <h1 className="font-semibold my-2">
+        - Ketidakmampuan Mengikuti Perkembangan Teknologi
+        </h1>
+
+        Bagi sebagian orang, Teknologi adalah sesuatu yang masih awam bagi mereka. Tidak semua kalangan "melek" teknologi. Sementara teknologi dapat memberikan solusi kesehatan dan konektivitas. Namun, beberapa lansia mungkin mengalami kesulitan dalam mengadopsi atau memahami teknologi tersebut. Pemahaman dan dukungan masyarakat untuk melibatkan lansia dalam dunia digital dapat membantu mengurangi kesenjangan generasional dan mengatasi masalah isolasi sosial.
+
+        <p className="mt-6">
+        Mengatasi tantangan ini membutuhkan kolaborasi lintas generasi dan pemahaman bahwa setiap fase kehidupan memiliki kebutuhan dan tantangan khusus terkait teknologi. Dengan pendekatan yang holistik dan inklusif, kita dapat menciptakan lingkungan di mana setiap generasi dapat memanfaatkan potensi positif teknologi sambil mengatasi tantangan yang mereka hadapi.
+        </p>
+
+        <p className="mt-10">
+          Pada Akhirnya, Dalam menghadapi gemerlapnya kemajuan teknologi, Kita dapat terbantu dengan banyaknya perkembangan yang semakin memudahkan kita. Namun, Pemahaman mengenai komplektifitas dinamika yang terkait dengan era digital juga perlu menjadi pertimbangan.
+
+          <p className="mt-4">
+          Pentingnya pendekatan holistik dan inklusif dalam mengatasi tantangan ini tidak dapat diabaikan. Kita perlu membentuk masyarakat yang memahami dan merangkul teknologi dengan bijaksana, mengajarkan etika digital sejak dini, memberdayakan pekerja dewasa dengan keterampilan baru, dan mendukung lansia agar tidak terpinggirkan.
+          </p>
+
+          <p className="mt-4">
+          Harmoni generasional dalam era teknologi bukan hanya mimpi, tetapi sebuah visi yang dapat diwujudkan melalui kerjasama dan pemahaman. Dengan pendekatan yang bijak, setiap generasi dapat merasakan manfaat teknologi sambil mengatasi tantangan yang unik bagi mereka. Melibatkan, mendukung, dan menghormati satu sama lain dalam perjalanan teknologi adalah kunci untuk menciptakan masyarakat yang inklusif dan berkelanjutan di zaman yang terus berubah ini.
+          </p>
+
+          <p className="mt-6">
+            Sekian.
+          </p>
+        </p>
+
+        </p>
+      </div>
+
+    </div>
+    </section>
+
+    </>
+  )
+}
+
+export default App
